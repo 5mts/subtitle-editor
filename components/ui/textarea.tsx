7a9788e9cae3a5e1e53ec265fc17246bba48cb27
David Purdy <davidpurdy@gmail.com>
2025-10-10 22:30:06 -0700

    Swap in react-textarea-autosize for vanilla HTML textarea in the textarea UI component

--- components/ui/textarea.tsx
+++ components/ui/textarea.tsx
@@ -1,13 +1,14 @@
 import * as React from "react";
 
 import { cn } from "@/lib/utils";
+import TextareaAutosize from "react-textarea-autosize";
 
 const Textarea = React.forwardRef<
   HTMLTextAreaElement,
-  React.ComponentProps<"textarea">
+  React.ComponentProps<typeof TextareaAutosize>
 >(({ className, ...props }, ref) => {
   return (
-    <textarea
+    <TextareaAutosize
       className={cn(
         "flex min-h-[60px] w-full rounded-md border border-input bg-transparent px-3 py-2 text-base shadow-xs placeholder:text-muted-foreground focus-visible:outline-hidden focus-visible:ring-1 focus-visible:ring-ring disabled:cursor-not-allowed disabled:opacity-50 md:text-lg",
         className
